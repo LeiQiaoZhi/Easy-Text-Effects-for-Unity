0a4b1979f405a4ce443a039c45912344a87478d3
nnra6864 <enenragd@gmail.com>
2025-06-07 16:59:56 +0200

    Implemented OnValueChanged for MoveEffectEditor

---
+++ Editor/MoveEffectEditor.cs
@@ -0,0 +1,17 @@
+using EasyTextEffects.Effects;
+using UnityEditor;
+
+namespace EasyTextEffects.Editor
+{
+    [CustomEditor(typeof(Effect_Move))]
+    public class MoveEffectEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var myScript = (Effect_Move)target;
+            EditorGUI.BeginChangeCheck();
+            DrawDefaultInspector();
+            if (EditorGUI.EndChangeCheck()) myScript.HandleValueChanged();
+        }
+    }
+}
